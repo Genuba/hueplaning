14f5dc854ce02afa39bfca0b5bf88824249ab61e
Ingenieros UE <ingenieria@uniempresarial.local>
2019-03-27 11:34:03 -0500

    hello

--- Implementation/php/index.php
+++ Implementation/php/index.php
@@ -1,6 +1,10 @@
 <?php 
-	error_reporting(E_ALL ^ E_NOTICE);
+	//error_reporting(E_ALL ^ E_NOTICE);
 	session_start();
+
+	require_once 'controller/UsuarioController.php';
+	$usuario = new UsuarioController();
+
 	if(!empty($_SESSION["id"])||$_GET["funcion"] == "login"){
 		require_once 'controller/ViewController.php';
 		require_once 'view/header.php';
@@ -12,6 +16,9 @@
 		$step = $_GET["step"];
 
 		new ViewController($page,$funcion,$step);
+
+		$result3 = $usuario->buscarUsuario($row[3])->fetch(PDO::FETCH_BOTH);
+										      		echo $result3[2];
 
 		require_once 'view/footer.php';
 	}else{
